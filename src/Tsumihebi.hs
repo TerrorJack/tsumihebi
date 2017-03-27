182bea314480d38ec9c866cb4a13fbbd1e93792b
Shao Cheng <astrohavoc@gmail.com>
2017-03-27 15:16:17 +0800

    Generate a mapping of header paths to contents, instead of concating.

--- src/Tsumihebi.hs
+++ src/Tsumihebi.hs
@@ -8,19 +8,28 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as LBS
 import qualified Data.ByteString.Char8 as BS (lines)
 import qualified Data.ByteString.Lazy.UTF8 as LBS
+import qualified Data.HashMap.Strict as HM
 import Language.Preprocessor.Cpphs
 import System.Directory.Extra
 import System.FilePath
 import System.IO
 import System.IO.Temp
 
-generateFakeHeader :: CpphsOptions -> FilePath -> IO String
+generateFakeHeader :: CpphsOptions
+                   -> FilePath
+                   -> IO (HM.HashMap FilePath String)
 generateFakeHeader opts bp = do
     ps <- listFilesRecursive bp
     bs <-
         forConcurrently [p | p <- ps, takeExtension p == ".h"] $ \p -> do
             f <- BS.readFile p
             pure $
+                HM.singleton
+                    [ if c == '\\'
+                        then '/'
+                        else c
+                    | c <- makeRelative bp p
+                    ] $
                 mconcat
                     [ LBS.byteString l `mappend` LBS.char7 '\n'
                     | l <- BS.lines f
@@ -29,7 +38,8 @@ generateFakeHeader opts bp = do
     withSystemTempFile "" $ \tmp h -> do
         hClose h
         res <-
-            runCpphs opts tmp $ LBS.toString $ LBS.toLazyByteString $ mconcat bs
+            forConcurrently (mconcat bs) $ \src ->
+                runCpphs opts tmp $ LBS.toString $ LBS.toLazyByteString src
         res `deepseq` pure res
 
 defOpts :: CpphsOptions
